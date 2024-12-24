2806472b4c00e16d17ea607113762a8e20885472
Zedude321 <55306539+zedude321@users.noreply.github.com>
2024-12-24 13:49:43 +0800

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 const Puzzle = require('./puzzle.model');
 const User = require('./user.model');
 const Room = require('./room.model');
-
-module.exports = { Puzzle, User, Room };
-=======
 const Item = require('./item.model');
-const Puzzle = require('./puzzle.model');
-const User = require('./user.model');
 
-module.exports = { Puzzle, User, Item };
->>>>>>> 7a69ab0 (Itemiig hiisen)
+module.exports = { Puzzle, User, Item, Room };
