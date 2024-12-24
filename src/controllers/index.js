a0e86b2654aa00cbe66fff586f075732f64465dd
Zedude321 <55306539+zedude321@users.noreply.github.com>
2024-12-24 13:48:51 +0800

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 const puzzleCtrls = require('./puzzle.controller')
 const userCtrls = require('./user.controller')
-const roomCtrls = require('./room.controller')
-
-module.exports = {...puzzleCtrls, ...userCtrls, ...roomCtrls}
-=======
-const puzzleCtrls = require('./puzzle.controller');
-const userCtrls = require('./user.controller');
+const roomCtrls = require('./room.controller');
 const itemCtrls = require('./item.controller');
 
-module.exports = { ...puzzleCtrls, ...userCtrls, ...itemCtrls };
->>>>>>> 7a69ab0 (Itemiig hiisen)
+module.exports = { ...puzzleCtrls, ...userCtrls, ...itemCtrls, ...roomCtrls };
